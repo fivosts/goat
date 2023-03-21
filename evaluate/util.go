dade2c9c7f515f161713d1aef78182551e7c8d26
Carlos Rosuero <carlos@rosuero.com>
2023-03-21 16:53:56 +0000

    Restore newlines

--- evaluate/util.go
+++ evaluate/util.go
@@ -1,74 +1,70 @@
 // Utility functions for package evaluate
 package evaluate
 
-import(
-  "time"
-  "log"
-  "os"
-  "bufio"
+import (
+	"bufio"
+	"log"
+	"os"
+	"time"
 )
 
+func ReadLines(f string) (lines []string) {
+	file, err := os.Open(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-func ReadLines(f string) (lines []string){
-  file, err := os.Open(f)
-  if err != nil {
-      log.Fatal(err)
-  }
-  defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-      lines = append(lines,scanner.Text())
-  }
-
-  if err := scanner.Err(); err != nil {
-      log.Fatal(err)
-  }
-  return lines
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
 }
 
-
-
 func MeasureTime(fn func()) (et time.Duration) {
-  start := time.Now()
-  fn()
-  end := time.Now()
-  et = end.Sub(start)
-  return et
+	start := time.Now()
+	fn()
+	end := time.Now()
+	et = end.Sub(start)
+	return et
 }
 
 func fileExist(filename string) bool {
-  _, err := os.Stat(filename)
-  return !os.IsNotExist(err)
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
 }
 
 func checkFile(filename string) bool {
-  fi, err := os.Stat(filename)
-  return !os.IsNotExist(err) && fi.Size()!=0
+	fi, err := os.Stat(filename)
+	return !os.IsNotExist(err) && fi.Size() != 0
 }
 
-
-func check(err error){
-	if err != nil{
+func check(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
 
 // If s contains e
 func contains(s []string, e string) bool {
-  for _, a := range s {
-    if a == e {
-      return true
-    }
-  }
-  return false
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
-func containsInt(l []int, b int) bool{
+func containsInt(l []int, b int) bool {
 	for _, a := range l {
-			if a == b {
-					return true
-			}
+		if a == b {
+			return true
+		}
 	}
 	return false
 }
